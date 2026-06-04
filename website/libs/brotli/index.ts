6757ccc45f4b5914a87ca861ca29e0920f57bad9
TimerErTim <timerertim@gmail.com>
2026-06-04 20:56:26 +0200

    refactor: remove brotli custom libs
    Not needed anymore after using simple brotli compression
    paired with vcdiff decoding

--- website/libs/brotli/index.ts
+++
@@ -1,4 +0,0 @@
-export { decompress, default } from "./decompress";
-export type { BrotliDecodeOptions } from "./types";
-export { State } from "./state";
-export * from "./errors";
--- website/libs/brotli/types.ts
+++
@@ -1,7 +0,0 @@
-export interface BrotliDecodeOptions {
-  /**
-   * Custom dictionary used during decompression. Must match the dictionary used
-   * when the stream was compressed. Pass `null` to use the built-in default.
-   */
-  customDictionary?: BufferSource | null;
-}
